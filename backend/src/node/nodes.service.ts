c1b3c6383235fbf3f6329829dab7f8d6d075efb7
Zeleo21 <leo.guazzaroni@epita.fr>
2026-02-08 10:56:35 +0000

    fix: fixed duplication bug and added virtual_root node

--- backend/src/node/nodes.service.ts
+++ backend/src/node/nodes.service.ts
@@ -3,9 +3,24 @@ import { FileInformation } from "./nodes.controller";
 import { FileEnum } from "../constants/fileEnum";
 import prisma from "../prisma/client";
 import { Prisma, Node } from '@prisma/client';
+import ROOT_NODE from "../prisma/constants";
 
 @Injectable()
 export class NodesService {
+
+    private dirCache = new Map<string, string>();
+
+    async checkVirtualRootNodeExistOrCreate(): Promise<void> {
+        await prisma.node.upsert({
+            where: { id: ROOT_NODE },
+            update: {},
+            create: {
+                id: ROOT_NODE,
+                name: "ROOT_VIRTUAL_NODE",
+                type: FileEnum.DIRECTORY,
+            }
+        })
+    }
 
     parsePath(path: string): string[] {
         if(!path) return [];
@@ -26,13 +41,23 @@ export class NodesService {
     }
 
     async upsertFilesIntoNodes(files: FileInformation[]): Promise<Node[]> {
+        await this.checkVirtualRootNodeExistOrCreate();
         let insertedFileNodes = [];
         for (const file of files) {
             const parsedPath = this.parsePath(file?.path);
             let directories = parsedPath.slice(0, parsedPath.length - 1);
             const currentFileParentId = await this.checkExistingDirectoriesOrCreate(directories);
             const newNodeForCurrentFile = this.transformFileToNode(file, currentFileParentId);
-            const newNode = await prisma.node.create({ data: newNodeForCurrentFile });
+            const newNode = await prisma.node.upsert({
+                where: {
+                    parentId_name: {
+                        name: newNodeForCurrentFile.name,
+                        parentId: newNodeForCurrentFile.parentId,
+                    }
+                },
+                update: {},
+                create: newNodeForCurrentFile,
+            });
             insertedFileNodes.push(newNode);
         }
         return insertedFileNodes;
@@ -40,8 +65,16 @@ export class NodesService {
 
     //We return the deepest directory parentId for it to be rattached to the currentFile.
     async checkExistingDirectoriesOrCreate(directories: string[]): Promise<string | null> {
+        console.log('directories', directories);
+        let pathKey = "";
         let currentParentId: string | null = null;
         for (const dirName of directories) {
+            pathKey += (pathKey ? '/' : '') + dirName;
+            if(this.dirCache.has(pathKey)) {
+                currentParentId = this.dirCache.get(pathKey)!;
+                continue;
+            }
+
             const node = await prisma.node.upsert({
                 where: {
                     parentId_name: {
@@ -53,10 +86,11 @@ export class NodesService {
                 create: {
                     name: dirName,
                     type: FileEnum.DIRECTORY,
-                    parentId: currentParentId
+                    parentId: currentParentId ?? "00000000-0000-0000-0000-000000000000"
                 }
             });
             currentParentId = node.id;
+            this.dirCache.set(pathKey, node.id);
         }
         return currentParentId;
     }
